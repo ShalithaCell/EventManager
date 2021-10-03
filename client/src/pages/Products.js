ae04a423a0f2e847c2ec5956f357a401efa6e3e5
Sasindu1997 <sasinduusgalhewa@gmail.com>
2021-10-03 17:22:06 +0530

    add event to mongo db - server

--- client/src/pages/Products.js
+++ client/src/pages/Products.js
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import axios from 'axios';
+
 // material
 import { Button, Typography, Container } from '@mui/material';
 
@@ -19,61 +21,71 @@ export default function EcommerceShop() {
   const [inputField, setInputField] = useState({
     summary: '',
     location: '',
-    description: ''
+    description: '',
+    imageURL: '',
+    start: '',
+    end: '',
+    attendees: [''],
+    category: ''
   });
 
   const inputsHandler = (e) => {
-    setInputField({ [e.target.name]: e.target.value });
+    setInputField({ ...inputField, [e.target.name]: e.target.value });
   };
 
-  const handleAddEvents = () => {
+  const handleAddEvents = (e) => {
+    console.log(inputField);
+
     console.log('Clicked');
-    gapi.load('client:auth2', () => {
-      console.log('Client loaded.');
-      gapi.client.init({
-        apiKey: API_KEY,
-        clientId: CLIENT_ID,
-        discoveryDocs: DISCOVERY_DOCS,
-        scope: SCOPES
-      });
 
-      gapi.client.load('calender', 'v3', () => console.log('Loaded'));
-      gapi.auth2
-        .getAuthInstance()
-        .signIn()
-        .then(() => {
-          const event = {
-            summary: inputField.summary,
-            location: inputField.location,
-            description: inputField.description,
-            start: {
-              dateTime: '2021-10-05T09:00:00-07:00',
-              timeZone: 'Asia/Colombo'
-            },
-            end: {
-              dateTime: '2021-10-06T17:00:00-07:00',
-              timeZone: 'Asia/Colombo'
-            },
-            recurrence: ['RRULE:FREQ=DAILY;COUNT=2'],
-            attendees: [{ email: 'lpage@example.com' }, { email: 'sbrin@example.com' }]
-            // reminders: {
-            //   useDefault: false,
-            //   overrides: [
-            //     { method: 'email', minutes: 24 * 60 },
-            //     { method: 'popup', minutes: 10 }
-            //   ]
-            // }
-          };
-          const request = gapi.client.calendar.events.insert({
-            calendarId: 'primary',
-            resource: event
-          });
+   
 
-          request.execute((event) => {
-            window.open(event.htmlLink);
-          });
-        });
-    });
+    // gapi.load('client:auth2', () => {
+    //   console.log('Client loaded.');
+    //   gapi.client.init({
+    //     apiKey: API_KEY,
+    //     clientId: CLIENT_ID,
+    //     discoveryDocs: DISCOVERY_DOCS,
+    //     scope: SCOPES
+    //   });
+
+    //   gapi.client.load('calender', 'v3', () => console.log('Loaded'));
+    //   gapi.auth2
+    //     .getAuthInstance()
+    //     .signIn()
+    //     .then(() => {
+    //       const event = {
+    //         summary: inputField.summary,
+    //         location: inputField.location,
+    //         description: inputField.description,
+    //         start: {
+    //           dateTime: '2021-10-05T09:00:00-07:00',
+    //           timeZone: 'Asia/Colombo'
+    //         },
+    //         end: {
+    //           dateTime: '2021-10-06T17:00:00-07:00',
+    //           timeZone: 'Asia/Colombo'
+    //         },
+    //         recurrence: ['RRULE:FREQ=DAILY;COUNT=2'],
+    //         attendees: [{ email: 'lpage@example.com' }, { email: 'sbrin@example.com' }]
+    //         // reminders: {
+    //         //   useDefault: false,
+    //         //   overrides: [
+    //         //     { method: 'email', minutes: 24 * 60 },
+    //         //     { method: 'popup', minutes: 10 }
+    //         //   ]
+    //         // }
+    //       };
+    //       const request = gapi.client.calendar.events.insert({
+    //         calendarId: 'primary',
+    //         resource: event
+    //       });
+
+    //       request.execute((event) => {
+    //         window.open(event.htmlLink);
+    //       });
+    //     });
+    // });
   };
 
   return (
@@ -85,34 +97,69 @@ export default function EcommerceShop() {
         <div>
           <input
             type="text"
-            name="first_name"
+            name="summary"
             onChange={inputsHandler}
-            placeholder="First Name"
-            value={inputField.first_name}
+            placeholder="Summary"
+            value={inputField.summary}
           />
-
           <br />
-
           <input
             type="text"
-            name="last_name"
+            name="location"
             onChange={inputsHandler}
-            placeholder="First Name"
-            value={inputField.last_name}
+            placeholder="Location"
+            value={inputField.location}
           />
-
           <br />
-
+          <input
+            type="text"
+            name="description"
+            onChange={inputsHandler}
+            placeholder="Description"
+            value={inputField.description}
+          />
+          <br />
+          <input
+            type="text"
+            name="imageURL"
+            onChange={inputsHandler}
+            placeholder="Image URL"
+            value={inputField.imageURL}
+          />
+          <br />
+          <input
+            type="date"
+            name="start"
+            onChange={inputsHandler}
+            placeholder="Start date and Time"
+            value={inputField.start}
+          />
+          <br />
+          <input
+            type="date"
+            name="end"
+            onChange={inputsHandler}
+            placeholder="End date and Time"
+            value={inputField.end}
+          />
+          <br />
           <input
             type="gmail"
             name="gmail"
             onChange={inputsHandler}
-            placeholder="Gmail"
+            placeholder="Attendies"
             value={inputField.gmail}
           />
-
           <br />
-
+          <input
+            type="text"
+            name="category"
+            onChange={inputsHandler}
+            placeholder="Category"
+            value={inputField.category}
+          />
+          <br />
+          <br />
           <Button onClick={handleAddEvents} variant="contained" size="large" className="m-10">
             Add Event
           </Button>
